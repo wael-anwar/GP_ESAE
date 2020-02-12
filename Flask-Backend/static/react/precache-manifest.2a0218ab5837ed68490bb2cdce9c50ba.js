350762080b8ac06584adfca4f64a1a24b1ed48e9
WaelAshraf <41231084+WaelAshraf@users.noreply.github.com>
2020-02-12 19:43:39 +0200

    main .py

---
+++ Flask-Backend/static/react/precache-manifest.2a0218ab5837ed68490bb2cdce9c50ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202bb0bd33b59af9a3b1040ba5937bcb",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "f317a19cc512bc42c7b8",
+    "url": "/static/react/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "44db4f6cbe10952ca77b",
+    "url": "/static/react/js/2.9a345854.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/react/js/2.9a345854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f317a19cc512bc42c7b8",
+    "url": "/static/react/js/main.68e5568d.chunk.js"
+  },
+  {
+    "revision": "165304c935f15a4707af",
+    "url": "/static/react/js/runtime-main.7ca781aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
