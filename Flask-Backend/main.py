c6cccc1b548b94ed17d7346e69a1b72ce3450a09
WaelAshraf <41231084+WaelAshraf@users.noreply.github.com>
2020-02-12 19:57:49 +0200

    complete setup

--- Flask-Backend/main.py
+++ Flask-Backend/main.py
@@ -4,6 +4,6 @@ app=flask.Flask("__main__")
 
 @app.route("/")
 def my_index():
-    return flask.render_template("index.html",token="Hello Flask+React")
+    return flask.render_template("index.html",token="Hello Flask+React (GP ESAE)")
 
 app.run(debug=True)
--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
   <p>My Token={window.token}</p>
-        <a
+        <a 
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
