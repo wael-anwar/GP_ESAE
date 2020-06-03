e6ed7bc923e6d73d33b4a402794db179f66a8cb2
AminGhassan <37709958+AminGhassan@users.noreply.github.com>
2020-06-03 21:14:32 +0200

    untracked

---
+++ Flask-Backend/static/react/precache-manifest.da146209f260406a3a45012ed3d908f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235dfee52b34d8ff2b723c5d76bd7bd3",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "4e5c6f1be9a38114fb20",
+    "url": "/static/react/css/main.bd21312f.chunk.css"
+  },
+  {
+    "revision": "3038b0867b20f1880c6f",
+    "url": "/static/react/js/2.b33f8b14.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/react/js/2.b33f8b14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e5c6f1be9a38114fb20",
+    "url": "/static/react/js/main.aeca3ee7.chunk.js"
+  },
+  {
+    "revision": "165304c935f15a4707af",
+    "url": "/static/react/js/runtime-main.7ca781aa.js"
+  },
+  {
+    "revision": "41de322b32bf52084623ce4fa4a7f10b",
+    "url": "/static/react/media/bg.41de322b.png"
+  }
+]);
